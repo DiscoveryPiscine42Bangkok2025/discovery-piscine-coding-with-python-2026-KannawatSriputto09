6e4dbdc921937a5d3bbf660943d1c0a7324a32bd
KannawatSriputto09 <67070009@kmitl.ac.th>
2026-02-12 00:51:23 +0700

    Create advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,9 @@
+"""ex03"""
+def main():
+    """main"""
+    for i in range(11):
+        print(f"Table de {i}:", end="")
+        for j in range(11):
+            print(f" {i * j}", end="")
+        print()
+main()

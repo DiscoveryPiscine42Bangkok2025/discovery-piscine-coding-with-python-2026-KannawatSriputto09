163e292ad7d7c9a4451eaf2a70a4ff6fa836bf94
KannawatSriputto09 <67070009@kmitl.ac.th>
2026-02-11 19:36:00 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+"""ex03"""
+def main():
+    """main"""
+    First_Name = input("Hey, what's your first name? : ")
+    Last_Name = input("And your last name? : ")
+    print(f"Well, pleased to meet you, {First_Name} {Last_Name}.")
+main()

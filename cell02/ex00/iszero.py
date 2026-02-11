4cd2fece8a970d6e17cf3f78c209c2ecfe919623
KannawatSriputto09 <67070009@kmitl.ac.th>
2026-02-11 19:56:04 +0700

    Create iszero.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,9 @@
+"""ex00"""
+def main():
+    """main"""
+    number = int(input())
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+main()

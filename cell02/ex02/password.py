8f88f6bb406fdf3805f15af7aadcbba2d9be198f
KannawatSriputto09 <67070009@kmitl.ac.th>
2026-02-11 22:12:17 +0700

    Create password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+"""ex02"""
+def main():
+    """main"""
+    password = "Python is awesome"
+    inputt = input()
+    if inputt == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED")
+main()
